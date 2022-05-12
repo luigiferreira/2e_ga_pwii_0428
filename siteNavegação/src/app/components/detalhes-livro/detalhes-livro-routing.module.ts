4a701978fefe5fa361dccb2da2ca05f20d938770
luigiferreira <luigiferreira456@gmail.com>
2022-05-12 14:41:13 +0000

    Alteração dos arquivos de rotas específicas das páginas: home, login e detalhes-livro

--- siteNavegação/src/app/components/detalhes-livro/detalhes-livro-routing.module.ts
+++ siteNavegação/src/app/components/detalhes-livro/detalhes-livro-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalhesLivroComponent } from './detalhes-livro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DetalhesLivroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

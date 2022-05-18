04f0877571b21441034373bbe3a4cfc65d8a0098
luigiferreira <luigiferreira456@gmail.com>
2022-05-18 17:32:19 +0000

    Início da criação dos atributos e métodos get e set da classe Autor

--- siteNavegação/src/app/models/Autor.ts
+++ siteNavegação/src/app/models/Autor.ts
@@ -1,3 +1,14 @@
-export class Autor{
-    
+export class Autor{    
+    private _nome: String;
+    nacionalidade: String;
+    sexo: String;
+    dataNascimento: Date;
+
+    public set nome(nome: String){
+        this._nome = nome;
+    }
+
+    public get nome(): String{
+        return this._nome;
+    }  
 }

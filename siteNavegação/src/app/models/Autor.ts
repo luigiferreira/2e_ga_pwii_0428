b573ddcc881292c9ad577938bd87810ecb190766
luigiferreira <luigiferreira456@gmail.com>
2022-05-18 17:33:51 +0000

    Conclusão da classe Autor

--- siteNavegação/src/app/models/Autor.ts
+++ siteNavegação/src/app/models/Autor.ts
@@ -1,14 +1,44 @@
 export class Autor{    
     private _nome: String;
-    nacionalidade: String;
-    sexo: String;
-    dataNascimento: Date;
+    private _nacionalidade: String;
+    private _sexo: String;
+    private _dataNascimento: Date;
+    
+    constructor(){
+        this._nome = "";
+        this._dataNascimento = new Date();
+        this._nacionalidade = "";
+        this._sexo = "";
+    }
 
     public set nome(nome: String){
         this._nome = nome;
     }
-
     public get nome(): String{
         return this._nome;
-    }  
+    }
+
+    public set nacionalidade(nacionalidade: String){
+        this._nacionalidade = nacionalidade;
+    }
+
+    public get nacionalidade(): String{
+        return this._nacionalidade;
+    }
+
+    public set sexo(sexo: String){
+        this._sexo = sexo;
+    }
+
+    public get sexo(): String{
+        return this._sexo;
+    }
+
+    public set dataNascimento(dataNascimento: Date){
+        this._dataNascimento = dataNascimento;
+    }
+
+    public get dataNascimento(): Date{
+        return this._dataNascimento;
+    }
 }
